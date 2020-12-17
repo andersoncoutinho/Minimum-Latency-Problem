73be4ebbce7a202637247bdbc669a8000ba94f53
Anderson da Silva Coutinho <andc876@gmail.com>
2020-12-17 10:35:39 -0300

    Update main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,38 @@
 #include "readData.h"
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <chrono>
 
 using namespace std;
+
+struct Solution {
+  vector <int> circuit;
+  double latency;
+};
 
 double ** matrizAdj; // matriz de adjacencia
 int dimension; // quantidade total de vertices
 
+extern void gils(Solution &solution, int dimension, double **matrizAdj);
+void printSolution(Solution solution, chrono::duration<double> executionTime);
 void printData();
 
 int main(int argc, char** argv) {
+  
+  srand(time(NULL));
+  readData(argc, argv, &dimension, &matrizAdj);
 
-    readData(argc, argv, &dimension, &matrizAdj);
-    printData();
-    return 0;
+  auto begin = chrono::system_clock::now();
 
-    
+  Solution solution;
+  gils(solution, dimension, matrizAdj);
+  auto end = chrono::system_clock::now();
+  chrono::duration<double> executionTime = end - begin;
+
+  printSolution(solution, executionTime);
+
+  return 0;  
 
 }
 
@@ -27,4 +44,17 @@ void printData() {
     }
     cout << endl;
   }
+}
+
+void printSolution(Solution solution, chrono::duration<double> executionTime) {
+  
+  for(auto i : solution.circuit) {
+    cout << solution.circuit[i] << ' ';
+  }
+
+  cout << endl;
+
+  cout << "Cost: " << solution.latency << endl;
+  cout << "Time: " << executionTime.count() << endl; 
+
 }
